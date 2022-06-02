74104ecacc5a49c92d8ae7c3b263d19869536cbe
laird-ikar <bguyot@student.42mulhouse.fr>
2022-06-02 08:16:27 +0200

    init contribution

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContributionModule } from './contribution/contribution.module';
 
 @Module({
 	imports: [
@@ -14,7 +15,8 @@ import { AppService } from './app.service';
 			database: 'bdedb',
 			entities: [],
 			synchronize: true
-		})
+		}),
+		ContributionModule
 	],
 	controllers: [AppController],
 	providers: [AppService],
