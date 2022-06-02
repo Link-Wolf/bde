12ed1ca147cd26d9dca941eff10c358e29b88b89
iCARUS <94838879+sur4c1@users.noreply.github.com>
2022-06-02 09:45:20 +0200

    controllers

--- src/contribution/contribution.controller.ts
+++ src/contribution/contribution.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Contribution } from './contribution.entity';
+import { ContributionService } from './contribution.service';
 
 @Controller('contribution')
-export class ContributionController {}
+export class ContributionController {
+	constructor(private contributionService: ContributionService) { }
+
+	@Get()
+	findAll(): Promise<Contribution[]> {
+		return this.contributionService.findAll();
+	}
+
+	@Get(':id')
+	findOne(@Param('id', ParseIntPipe) id: number): Promise<Contribution> {
+		return this.contributionService.findOne(id);
+	}
+
+	@Post()
+	create(@Body() contribution: Contribution) {
+		this.contributionService.create(contribution);
+	}
+
+	@Delete(':id')
+	remove(@Param('id', ParseIntPipe) id: number) {
+		this.contributionService.remove(id);
+	}
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { User } from './user.entity';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+	constructor(private UserService: UserService) { }
+
+	@Get()
+	findAll(): Promise<User[]> {
+		return this.UserService.findAll();
+	}
+
+	@Get(':login')
+	findOne(@Param('login') login: string): Promise<User> {
+		return this.UserService.findOne(login);
+	}
+
+	@Post()
+	create(@Body() user: User) {
+		this.UserService.create(user);
+	}
+
+	@Delete(':login')
+	remove(@Param('login') login: string) {
+		this.UserService.remove(login);
+	}
+}
