2220b85c9d0488f256b54c5cb8841ba846a93d6b
iCARUS <94838879+sur4c1@users.noreply.github.com>
2022-06-02 07:36:48 +0200

    init ORM + user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host: 'localhost',
+			port: 4343,
+			username: 'root',
+			password: 'root',
+			database: 'bdedb',
+			entities: [],
+			synchronize: true
+		}),
+		UserModule
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
