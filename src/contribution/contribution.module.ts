da999fabbaac5e552599c348e8923544f32deb97
iCARUS <94838879+sur4c1@users.noreply.github.com>
2022-06-03 15:53:53 +0200

    working way better date than before

--- src/contribution/contribution.module.ts
+++ src/contribution/contribution.module.ts
@@ -3,9 +3,10 @@ import { ContributionService } from './contribution.service';
 import { ContributionController } from './contribution.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Contribution } from '../entity/Contribution';
+import { UserModule } from '../user/user.module';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Contribution])],
+	imports: [TypeOrmModule.forFeature([Contribution]), UserModule],
 	providers: [ContributionService],
 	controllers: [ContributionController]
 })
--- src/entity/Contribution.ts
+++ src/entity/Contribution.ts
@@ -1,5 +1,5 @@
 import { User } from "./User";
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
 
 @Entity()
 export class Contribution {
