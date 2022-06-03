191caca0aecf558e77f925a5df7ac5302cdeeca9
laird-ikar <bguyot@student.42mulhouse.fr>
2022-06-03 16:35:19 +0200

    event services is now functionnal

--- src/event/event.service.ts
+++ src/event/event.service.ts
@@ -2,27 +2,40 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Event } from '../entity/Event'
+import { EventDto } from './event.dto';
 
 @Injectable()
 export class EventService {
 	constructor(
 		@InjectRepository(Event)
-		private usersRepository: Repository<Event>,
+		private eventRepository: Repository<Event>,
 	) { }
 
 	findAll(): Promise<Event[]> {
-		return this.usersRepository.find();
+		return this.eventRepository.find();
 	}
 
 	findOne(id: number): Promise<Event> {
-		return this.usersRepository.findOneBy({ id: id });
+		return this.eventRepository.findOneBy({ id: id });
 	}
 
-	async create(event: Event): Promise<void> {
-		this.usersRepository.save(event);
+	async create(eventDto: EventDto): Promise<void> {
+		const event = {
+			name: eventDto.name,
+			cost: Number(eventDto.cost),
+			premium_cost: Number(eventDto.premium_cost || eventDto.cost),
+			nb_places: Number(eventDto.nb_places)
+		}
+		console.log("Create the event :")
+		console.log(event)
+		await this.eventRepository.save(event);
 	}
 
-	async remove(id: number): Promise<void> {
-		await this.usersRepository.delete({ id: id });
+	async removeOne(id: number): Promise<void> {
+		await this.eventRepository.delete({ id: id });
+	}
+
+	async removeAll(): Promise<void> {
+		await this.eventRepository.delete({});
 	}
 }
