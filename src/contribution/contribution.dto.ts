99e544baa9ce486c8e5b31cee4227d2185c1244c
laird-ikar <bguyot@student.42mulhouse.fr>
2022-06-03 15:53:53 +0200

    working way better date than before

--- src/contribution/contribution.dto.ts
+++ src/contribution/contribution.dto.ts
@@ -1,4 +1,3 @@
 export class ContributionDto {
 	user: string
-	cost: string
 }
--- src/contribution/contribution.service.ts
+++ src/contribution/contribution.service.ts
@@ -2,39 +2,44 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Contribution } from '../entity/Contribution';
+import { UserService } from '../user/user.service';
 import { ContributionDto } from './contribution.dto';
 
 @Injectable()
 export class ContributionService {
 	constructor(
 		@InjectRepository(Contribution)
-		private usersRepository: Repository<Contribution>,
+		private contributionRepository: Repository<Contribution>,
+		private userService: UserService
 	) { }
 
 	findAll(): Promise<Contribution[]> {
-		return this.usersRepository.find();
+		return this.contributionRepository.find();
 	}
 
 	findOne(userLogin: string): Promise<Contribution> {
-		return this.usersRepository.findOneBy({ userLogin: userLogin });
+		return this.contributionRepository.findOneBy({ userLogin: userLogin });
 	}
 
 	async create(contribution: ContributionDto): Promise<void> {
 		let today = new Date(Date.now());
 		let due = new Date(Date.now());
 		due.setMonth(due.getMonth() + 6)
-		this.usersRepository.save({
+		await this.contributionRepository.save({
+			userLogin: contribution.user,
 			begin_date: today,
-			cost: Number(contribution.cost),
+			cost: 10,
 			end_date: due,
+			user: await this.userService
+				.findOne(contribution.user)
 		});
 	}
 
 	async removeOne(userLogin: string): Promise<void> {
-		await this.usersRepository.delete({ userLogin: userLogin });
+		await this.contributionRepository.delete({ userLogin: userLogin });
 	}
 
 	async removeAll(): Promise<void> {
-		await this.usersRepository.delete({});
+		await this.contributionRepository.delete({});
 	}
 }
