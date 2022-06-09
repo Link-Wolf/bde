95873307dbc3da87b5ed6494d3bc17b16dc572b8
laird-ikar <bguyot@student.42mulhouse.fr>
2022-06-09 16:41:24 +0200

    FOCK

--- src/logger/CustomLogger.ts
+++
@@ -1,8 +0,0 @@
-import { Logger } from '@nestjs/common';
-
-export class CustomLogger extends Logger {
-	error(message: string, trace: string) {
-		// write the message to a file, send it to the database or do anything
-		super.error(message, trace);
-	}
-}
