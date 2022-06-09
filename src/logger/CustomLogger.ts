7b708526a0bb5d212914d8de79093f442a4e7531
Link <94838493+Link-Wolf@users.noreply.github.com>
2022-06-09 13:56:48 +0200

    begin custom logger

--- src/logger/CustomLogger.ts
+++ src/logger/CustomLogger.ts
@@ -1,8 +1,21 @@
-import { Logger } from '@nestjs/common';
+import { ConsoleLogger } from '@nestjs/common';
 
-export class CustomLogger extends Logger {
-	error(message: string, trace: string) {
-		// write the message to a file, send it to the database or do anything
-		super.error(message, trace);
-	}
+export class CustomLogger extends ConsoleLogger {
+  async error(message: any, stack?: string, context?: string) {
+    await this.logRepository.save({message: message, type: "error"})
+    super.error(message, stack, context);
+  }
+  async warn(message: any, stack?: string, context?: string) {
+	await this.logRepository.save({message: message, type: "warn"})
+	super.warn(message, stack, context);
+  }
+  log(message: any, stack?: string, context?: string) {
+    super.log(message, stack, context);
+  }
+  verbose(message: any, stack?: string, context?: string) {
+    super.verbose(message, stack, context);
+  }
+  debug(message: any, stack?: string, context?: string) {
+    super.debug(message, stack, context);
+  }
 }
