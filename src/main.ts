03babab672f8eff69428db4b620dede6ae63d76c
iCARUS <94838879+sur4c1@users.noreply.github.com>
2022-06-09 16:36:52 +0200

    stuff?

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, {
-		logger: ['debug']
-	});
-	await app.listen(4242);
+	const app = await NestFactory.create(AppModule,
+		{
+			logger: ['debug']
+		});
+	await app.listen(3000);
 }
 bootstrap();
