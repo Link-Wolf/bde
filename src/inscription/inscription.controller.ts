cd47e284f95ac7d9a6a5f17e4a76efc93dce0163
laird-ikar <bguyot@student.42mulhouse.fr>
2022-06-09 08:01:10 +0200

    clean unused imports

--- src/inscription/inscription.controller.ts
+++ src/inscription/inscription.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { InscriptionService } from './inscription.service';
 
 @Controller('inscription')
