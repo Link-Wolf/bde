ac083b2a56cb5e4edd057a9fc3de9a441ba614ec
iCARUS <94838879+sur4c1@users.noreply.github.com>
2022-06-09 08:01:10 +0200

    clean unused imports

--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -1,9 +1,8 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Redirect } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { EventService } from './event.service';
 import { Event } from '../entity/Event'
 import { EventDto } from './event.dto';
 import { EventDtoPipe } from './event.pipe';
-import { InscriptionService } from '../inscription/inscription.service';
 
 @Controller('event')
 export class EventController {
