eee4c8c1fcbe04ec0ac232fb50bfa2e4dd539498
iCARUS <94838879+sur4c1@users.noreply.github.com>
2022-06-10 12:29:34 +0200

    import loggermodule

--- src/stud/stud.module.ts
+++ src/stud/stud.module.ts
@@ -3,9 +3,10 @@ import { StudService } from './stud.service';
 import { StudController } from './stud.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Stud } from '../entity/Stud';
+import { LoggerModule } from '../logger/logger.module';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Stud])],
+	imports: [TypeOrmModule.forFeature([Stud]), LoggerModule],
 	providers: [StudService],
 	controllers: [StudController],
 	exports: [StudService]
