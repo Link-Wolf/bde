505d1c008eb75480cf80d9050eda49325b616234
laird-ikar <bguyot@student.42mulhouse.fr>
2022-06-10 13:11:47 +0200

    log in contribution : done =)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './http-exception.filter';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule,
 		{
 			logger: ['debug']
 		});
+	app.useGlobalFilters(new HttpExceptionFilter());
 	await app.listen(4242);
 }
 bootstrap();
