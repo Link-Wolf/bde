e3d073a239a5edf89624a2fc8fa94cac9d49ce33
laird-ikar <bguyot@student.42mulhouse.fr>
2022-06-10 10:55:14 +0200

    undo logger

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule,
+		{
+			logger: ['debug']
+		});
+	await app.listen(4242);
+}
+bootstrap();
