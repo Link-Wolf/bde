861b1c8615e97faeb10eb4dfd935af53e29f76e3
iCARUS <94838879+sur4c1@users.noreply.github.com>
2022-06-14 08:16:42 +0200

    following a random guys advice and changing the target

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,20 +1,20 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-local";
-import { AuthService } from "./auth.service";
+
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-	constructor(
-		private authService: AuthService
-	) {
+	constructor(private authService: AuthService) {
 		super();
 	}
 
-	async validate(login: string, password: string): Promise<any> {
-		const user = await this.authService.validateUser(login, password);
-		if (!user)
+	async validate(username: string, password: string): Promise<any> {
+		const user = await this.authService.validateUser(username, password);
+		if (!user) {
 			throw new UnauthorizedException();
+		}
 		return user;
 	}
 }
