8de84cce79590c03e30cfe13fbb8b008696e459a
Link <wolflink6859@gmail.com>
2022-06-15 16:34:48 +0200

    add csurf, helmet et wip cookies

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './http-exception.filter';
+import helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule,
 		{
 			logger: ['debug']
 		});
+	app.use(helmet(), csurf(), cookieParser("hi this is the secret"))
 	app.useGlobalFilters(new HttpExceptionFilter());
 	await app.listen(4242);
 }
