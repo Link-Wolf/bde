c7f30e19e6c3611fbc201649f8977c21154691ac
Link <94838493+Link-Wolf@users.noreply.github.com>
2022-06-17 17:25:20 +0200

    structure du home bitches

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
 
-function Home () {
-  	return(
-		<h1> Home </h1>
-	)
-};
+function Home() {
+	return (
+		<div>
+			<div> Intro here </div>
+			<div> Album custom all events </div>
+			<div>
+				Events en cours et a venir
+				<ul>
+					<li> Event 1 </li>
+					<li> Event 2 </li>
+					<li> Event 3 </li>
+				</ul>
+			</div>
+		</div>
+	);
+}
 
 export default Home;
