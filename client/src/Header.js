a37f6eadb353f3cc9b8fc9e3bff630fbaba061f6
laird-ikar <bguyot@student.42mulhouse.fr>
2022-06-17 14:31:03 +0200

    headerrrrrrrrrrrrr

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,11 +1,36 @@
 function Header() {
 	return (
-			<>
-			<a href=".."> Home </a>
-			<a href="../stud"> Stud </a>
-			<a href="../event"> Event </a>
-			</>
+		<div>
+			<div>
+				<h1> BDE </h1>
+			</div>
+			<div class="menu">
+				<ul class="drop-down menu">
+					<li>
+						<ul>
+							<a href=".."> Home </a>
+						</ul>
+						<ul>
+							<a href="../event"> Event </a>
+						</ul>
+						<ul>
+							<a href="../shop"> Shop </a>
+						</ul>
+						<ul>
+							<li>
+								<ul>
+									<a href="../profile"> Profile </a>
+								</ul>
+								<ul>
+									<a href="../login"> Login/Logout </a>
+								</ul>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
 	);
-};
+}
 
 export default Header;
