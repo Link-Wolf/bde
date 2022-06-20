e14a86466c6f60fd89f964f2e6f81fa0dfc99812
Link <94838493+Link-Wolf@users.noreply.github.com>
2022-06-20 12:24:15 +0200

    intro sexy

--- client/src/pages/body/Home.js
+++ client/src/pages/body/Home.js
@@ -3,8 +3,15 @@ import React from "react";
 function Home() {
 	return (
 		<div>
-			<div> Intro here </div>
-			<div> Album custom all events </div>
+			<div>
+				Ouais salut c'est le BDE
+				<br /> Ici on est pas la pour faire des oeufs
+				<br />
+				Alors accroche toi bien a ton iMac
+				<br />
+				Parce qu'on va casser la baraque
+			</div>
+			<div> [ Album custom all events dans le petit carroussel ]</div>
 			<div>
 				Events en cours et a venir
 				<ul>
