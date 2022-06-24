505fd33fd3f9cc9ff9196d92271d9d32fc3be0f6
iCARUS <94838879+sur4c1@users.noreply.github.com>
2022-06-24 10:43:14 +0200

    Sorting checkers

---
+++ client/src/components/CheckSet.js
@@ -0,0 +1,22 @@
+const CheckSet = param => {
+	return (
+		<fieldset>
+			{param.set.map(item => {
+				return (
+					<div>
+						<input
+							type={param.type}
+							name="filter"
+							defaultChecked={item.defaultChecked}
+							value={item.value}
+							onChange={param.onChange}
+						/>
+						<label>{item.name}</label>
+					</div>
+				);
+			})}
+		</fieldset>
+	);
+};
+
+export default CheckSet;
--- client/src/pages/body/Events.js
+++ client/src/pages/body/Events.js
@@ -1,19 +1,69 @@
-import React from "react";
+import React, {useState} from "react";
+import EventList from "../../components/EventList";
+import CheckSet from "../../components/CheckSet";
+import {Dropdown} from "react-bootstrap";
 
 const Event = () => {
+	const [filter, setFilter] = useState({});
+
 	return (
-		<div>
+		<div className={`${"flex"}`}>
 			<div>
-				Events a venir
-				<div>
-					<div>Filtres</div>
-					<div>Tri</div>
-					<ul>
-						<li>Event 1</li>
-						<li>Event 2</li>
-						<li>Event 3</li>
-					</ul>
-				</div>
+				<Dropdown>
+					<Dropdown.Toggle variant="success" id="dropdown-basic">
+						Filtre
+					</Dropdown.Toggle>
+
+					<Dropdown.Menu>
+						<CheckSet
+							set={[
+								{name: "Current", value: "current"},
+								{name: "Free", value: "free"},
+								{name: "Available", value: "available"}
+							]}
+							onChange={null}
+							type="checkbox"
+						/>
+					</Dropdown.Menu>
+				</Dropdown>
+				<Dropdown>
+					<Dropdown.Toggle variant="success" id="dropdown-basic">
+						Tri
+					</Dropdown.Toggle>
+					<Dropdown.Menu>
+						<CheckSet
+							set={[
+								{
+									name: "Begin Date",
+									value: "begin_date",
+									defaultChecked: true
+								},
+								{name: "End Date", value: "end_date"},
+								{name: "ID", value: "id"},
+								{name: "Nombre de place", value: "nb_places"},
+								{name: "Nom", value: "name"},
+								{name: "Lieu", value: "place"},
+								{name: "Prix", value: "cost"}
+							]}
+							onChange={null}
+							type="radio"
+						/>
+						<hr />
+						<CheckSet
+							set={[
+								{
+									name: "Desc",
+									value: "desc",
+									defaultChecked: true
+								},
+								{name: "Asc", value: "asc"}
+							]}
+							onChange={null}
+							type="radio"
+						/>
+					</Dropdown.Menu>
+				</Dropdown>
+				<EventList filter={filter} />
 			</div>
 			<div>
 				Album
