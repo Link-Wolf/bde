e55882dc06778488da9324d41c5750fcda087d12
laird-ikar <bguyot@student.42mulhouse.fr>
2022-06-28 13:44:13 +0200

    pretty header ongoing

---
+++ client/src/components/Dropdown.js
@@ -0,0 +1,21 @@
+import {useState} from "react";
+
+const Dropdown = param => {
+	const [deployed, setDeployed] = useState(false);
+
+	const handleButton = () => {
+		setDeployed(!deployed);
+	};
+
+	let content = <div> {param.children} </div>;
+	if (!deployed) content = "";
+
+	return (
+		<>
+			<button onClick={handleButton}>{param.title}</button>
+			{content}
+		</>
+	);
+};
+
+export default Dropdown;
