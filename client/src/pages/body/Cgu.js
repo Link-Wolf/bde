21a74d8f70e83bc65c15d27916f7c610b0b25b98
Link <94838493+Link-Wolf@users.noreply.github.com>
2022-06-30 10:58:03 +0200

    Begin CGU

--- client/src/pages/body/Cgu.js
+++ client/src/pages/body/Cgu.js
@@ -1,7 +1,88 @@
 import React from "react";
 
 const Cgu = () => {
-	return <h1> CGU </h1>;
+	return (
+		<>
+			<h1>Conditions générales d'utilisations</h1>
+			<p>
+				Conformément aux dispositions de la loi n° 2004-575 du 21 juin
+				2004 pour la confiance en l’économie numérique, il est précisé
+				aux utilisateurs et visiteurs, ci après l'"Utilisateur" du site
+				bde42mulhouse.fr, ci après le "Site", les présentes mentions
+				légales stipulant notamment l’identité des différents
+				intervenants dans le cadre de sa réalisation et de son suivi.
+			</p>
+			<p>
+				La connexion et la navigation sur le Site par l'Utilisateur
+				implique l'acceptation intégrale et sans réserves des présentes
+				mentions légales par ce dernier.
+			</p>
+			<p>
+				<h2>Edition</h2>
+				<p>
+					Le Site est le site web du BDE de 42 Mulhouse. Il permet
+					l'accès aux évènements organisés par ce dernier et le
+					paiement des cotisations pour les étudiants connectés. Pour
+					l'Utilisateur non connecté, l'accès se résume principalement
+					à une vitrine du BDE et de ses évènements, ainsi qu'à prise
+					de contact avec ce dernier.
+				</p>
+				<ul>
+					<li>
+						Directeur de la publication, ci après l'"Editeur" :
+						Allan ?
+					</li>
+					<li>Adresse : 30 Rue François Spoerry, 68100 Mulhouse</li>
+					<li>
+						Responsabilité technique, conception et développement :
+						### ### et Xxxxx XXXXXX
+					</li>
+				</ul>
+			</p>
+			<p>
+				<h2>Hébergement</h2>
+				<p>
+					L'hébergeur du Site est l'institut de formation 42 Mulhouse
+					à l'adresse suivante : 30 Rue François Spoerry, 68100
+					Mulhouse, pouvant être contactée au 0642694269 et joint par
+					mail via [mail de 42].
+				</p>
+			</p>
+			<p>
+				<h2>Nom de domaine</h2>
+				<p>
+					Le nom de domaine est la propriété exclusive du BDE de 42
+					Mulhouse. La réservation du nom de domaine "bde42mulhouse.fr
+					" a été effectuée auprès de l'AFNIC conformément aux
+					dispositions en vigueur. L'attribution et le retrait des
+					noms de sous-domaines sont du ressort exclusif du BDE 42
+					Mulhouse.
+				</p>
+			</p>
+			<p>
+				<h2>Accès au site</h2>
+				<p>
+					Le Site est accessible 7 jours sur 7 et 24 heures sur 24 en
+					tout lieu, excepté en cas de force majeure, de maintenance
+					annoncée ou non ou d'interruption annoncée, temporaire ou
+					définitive, des services. Dans ces cas, l'Editeur ne saurait
+					être tenu responsable de modifications, alterations,
+					interruptions ou suspensions du Site.
+				</p>
+			</p>
+			<p>
+				<h2>Propriété intelectuelle</h2>
+				<p>
+					Wallah ca commence a me gonfler les CGU et encore j'ai pas
+					encore commencé les CGV
+				</p>
+			</p>
+			<p>
+				<h2>Nous contacter</h2>
+				<p>Par email : contact.bde42mulhouse@gmail.com</p>
+			</p>
+		</>
+	);
 };
 
 export default Cgu;
