85a88629b951c0b3c34df4be9fa66ea7df7261bb
laird-ikar <bguyot@student.42mulhouse.fr>
2022-07-04 11:42:50 +0200

    bootstrap ton header

--- client/src/pages/Header.js
+++ client/src/pages/Header.js
@@ -1,45 +1,45 @@
-import Dropdown from "../components/Dropdown";
+import {Navbar, Nav, NavDropdown, Container} from "react-bootstrap";
 
 import bde_logo from "../images/bde_logo.webp";
+import user_picture from "../images/user_placeholder.png";
 
-import style from "../style/Header.module.css";
-
-window.onscroll = function() {
-	scrollFunction();
-};
-
-function scrollFunction() {
-	if (
-		document.body.scrollTop > 50 ||
-		document.documentElement.scrollTop > 50
-	) {
-		document.documentElement.style.setProperty("--header_height", "5vw");
-	} else {
-		document.documentElement.style.setProperty("--header_height", "10vw");
-	}
-}
+import secret from "../secret.json";
 
 function Header() {
 	return (
-		<header className={style.header} id="header">
-			<a href="/home">
-				<img id={style.bde_logo} src={bde_logo} />
-			</a>
-			<a href="/events">
-				<button> Events </button>
-			</a>
-			<a href="/shop">
-				<button> Shop </button>
-			</a>
-			<a href="/admin">
-				<button> Admin </button>
-			</a>
-			<Dropdown title="User" id={style.loginButton}>
-				<a href="/me"> Profile </a>
-				<a href="/settings"> Settings </a>
-				<a href="/log"> Login/Logout </a>
-			</Dropdown>
-		</header>
+		<Navbar sticky="top" collapseOnSelect bg="dark" variant="dark">
+			<Container>
+				<Navbar.Brand href="/home">
+					<img
+						height="30"
+						width="30"
+						className="d-inline-block align-top"
+						src={bde_logo}
+					/>
+					<Navbar.Text>Fregate</Navbar.Text>
+				</Navbar.Brand>
+				<Nav className="me-auto">
+					<Nav.Link href="/events">Events</Nav.Link>
+					<Nav.Link href="/shop">Shop</Nav.Link>
+				</Nav>
+				<Nav>
+					<Nav.Link href="/admin">Admin</Nav.Link>
+					<NavDropdown
+						title={
+							<img width="30" height="30" src={user_picture} />
+						}
+					>
+						<NavDropdown.Item href="/me">Profile</NavDropdown.Item>
+						<NavDropdown.Item href="/settings">
+							Settings
+						</NavDropdown.Item>
+						<NavDropdown.Item href={secret.intra_url}>
+							Login
+						</NavDropdown.Item>
+					</NavDropdown>
+				</Nav>
+			</Container>
+		</Navbar>
 	);
 }
 
