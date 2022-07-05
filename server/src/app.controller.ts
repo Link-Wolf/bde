2f8bb2425a2a5d4e35fcf39aa98cd5d790324431
laird-ikar <bguyot@student.42mulhouse.fr>
2022-07-05 09:20:41 +0200

    fuck go back

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get, Post, Res, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { LocalAuthGuard } from './auth/localAuth.guard';
+import { Public } from './auth/public.decorator';
 
 @Controller()
 export class AppController {
 	constructor(
-		private readonly appService: AppService, ) { }
+		private readonly appService: AppService,
+		private readonly authService: AuthService) { }
 
 	@Get()
 	getHello(@Res() response: any, @Req() req: any) {
@@ -14,5 +18,12 @@ export class AppController {
 		response.cookie('test2', 'yeet')
 		console.log('Cookies: ', response.cookies)
 		console.log('Signed Cookies: ', response.signedCookies)
+	}
+
+	@Public()
+	@UseGuards(LocalAuthGuard)
+	@Post('auth/login')
+	async login(@Req() req: any) {
+		return this.authService.login(req.user);
 	}
 }
