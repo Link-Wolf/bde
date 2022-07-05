24e9ece139df88419a4d692b77fdf68309d779b1
laird-ikar <bguyot@student.42mulhouse.fr>
2022-07-05 07:52:15 +0200

    clearing old auth tests + inits new clean auth + log page front side

---
+++ client/src/pages/body/Log.js
@@ -0,0 +1,32 @@
+import {useSearchParams} from "react-router-dom";
+import {useState} from "react";
+
+const Log = () => {
+	const [searchParams] = useSearchParams();
+	const [ret, setRet] = useState();
+
+	const code = searchParams.get("code");
+	const requestOptions = {
+		method: "post",
+		headers: {"Content-Type": "application/json"}, //add security token here i guess
+		body: JSON.stringify({
+			code: code
+		})
+	};
+	fetch("http://k1r2p10.42mulhouse.fr:4242/auth", requestOptions)
+		.then(response => {
+			if (!response.ok) {
+				throw new Error(
+					`This is an HTTP error: The status is ${response.status}`
+				);
+			}
+			return response.json();
+		})
+		.catch(function(error) {
+			console.log(
+				"Il y a eu un problème avec l'opération fetch: " + error.message
+			);
+		});
+};
+
+export default Log;
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,25 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { StudService } from '../stud/stud.service';
 
 @Injectable()
-export class AuthService {
-	constructor(
-		private studService: StudService,
-		private jwtService: JwtService
-	) { }
-
-	async validateUser(login: string, password: string): Promise<any> {
-		const stud = await this.studService.findOne(login);
-		if (stud && password === "pass")
-			return stud;
-		return null;
-	}
-
-	async login(user: any) {
-		const payload = { user: user };
-		return {
-			access_token: this.jwtService.sign(payload),
-		};
-	}
-}
+export class AuthService {}
