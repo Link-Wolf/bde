b76f32e3bad7ad019be29843167ceeea6160c00b
laird-ikar <bguyot@student.42mulhouse.fr>
2022-07-05 14:26:43 +0200

    we are now getting the token in the other side

--- client/src/pages/body/Log.js
+++ client/src/pages/body/Log.js
@@ -1,32 +1,40 @@
 import {useSearchParams} from "react-router-dom";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 const Log = () => {
 	const [searchParams] = useSearchParams();
-	const [ret, setRet] = useState();
+	const [data, setData] = useState({});
 
-	const code = searchParams.get("code");
-	const requestOptions = {
-		method: "post",
-		headers: {"Content-Type": "application/json"}, //add security token here i guess
-		body: JSON.stringify({
-			code: code
-		})
-	};
-	fetch("http://k1r2p10.42mulhouse.fr:4242/auth", requestOptions)
-		.then(response => {
-			if (!response.ok) {
-				throw new Error(
-					`This is an HTTP error: The status is ${response.status}`
+	useEffect(() => {
+		const code = searchParams.get("code");
+		const requestOptions = {
+			method: "post",
+			headers: {"Content-Type": "application/json"}, //add security token here i guess
+			body: JSON.stringify({
+				code: code
+			})
+		};
+		fetch("http://k1r2p10.42mulhouse.fr:4242/auth", requestOptions)
+			.then(response => {
+				if (!response.ok) {
+					throw new Error(
+						`This is an HTTP error: The status is ${response.status}`
+					);
+				}
+				return response.json();
+			})
+			.then(actualData => {
+				console.log(actualData);
+				setData(actualData);
+			})
+			.catch(function(error) {
+				console.log(
+					"Il y a eu un problème avec l'opération fetch: " +
+						error.message
 				);
-			}
-			console.log(response);
-		})
-		.catch(function(error) {
-			console.log(
-				"Il y a eu un problème avec l'opération fetch: " + error.message
-			);
-		});
+			});
+		console.log(data);
+	}, []);
 };
 
 export default Log;
