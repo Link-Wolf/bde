5955d6f81d9160c46c16df33c43619e05d4fda83
iCARUS <94838879+sur4c1@users.noreply.github.com>
2022-08-01 13:12:49 +0200

    the maillling

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -36,6 +36,12 @@ const Logout = () => {
 			})
 			.then(() => {
 				setRet(<Navigate to="/home" replace={true} />);
+			})
+			.catch(function(error) {
+				console.log(
+					"Il y a eu un problème avec l'opération fetch: " +
+						error.message
+				);
 			});
 	}, []);
 
