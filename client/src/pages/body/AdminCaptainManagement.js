ea877d4d5d3a8b662aa0d712b0c55b3de4ebe6fa
Link <94838493+Link-Wolf@users.noreply.github.com>
2022-08-01 13:14:03 +0200

    crier better

--- client/src/pages/body/AdminCaptainManagement.js
+++ client/src/pages/body/AdminCaptainManagement.js
@@ -31,7 +31,7 @@ const AdminCaptainManagement = param => {
 
 	const yeetConfirm = toYeet => {
 		const requestOptions = {
-			method: "patch",
+			method: "PATCH",
 			credentials: "include",
 			headers: {
 				"Content-Type": "application/json"
