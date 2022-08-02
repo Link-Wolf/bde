e4c93c316961a6b7a326173ec5d58e8f68941be1
Link <94838493+Link-Wolf@users.noreply.github.com>
2022-08-02 15:21:40 +0200

    better scroll

--- client/src/App.js
+++ client/src/App.js
@@ -42,11 +42,3 @@ function App() {
 }
 
 export default App;
-
-// import { Link } from "react-router-dom";
-// ...
-// <Link to="/signup">
-//   <button variant="outlined">
-//     Sign up
-//   </button>
-// </Link>
