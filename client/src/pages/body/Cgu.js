a4244b577671fa75dd440f4e5c974b5105ec7501
Link <94838493+Link-Wolf@users.noreply.github.com>
2022-08-10 12:53:47 +0200

    update + changement de poste

--- client/src/pages/body/Cgu.js
+++ client/src/pages/body/Cgu.js
@@ -8,8 +8,8 @@ const Cgu = () => {
 				Conformément aux dispositions de la loi n° 2004-575 du 21 juin
 				2004 pour la confiance en l’économie numérique, il est précisé
 				aux utilisateurs et visiteurs, ci après l'"Utilisateur" du site
-				bde42mulhouse.fr, ci après le "Site", les présentes mentions
-				légales stipulant notamment l’identité des différents
+				<b>bde42mulhouse.fr</b>, ci après le "Site", les présentes
+				mentions légales stipulant notamment l’identité des différents
 				intervenants dans le cadre de sa réalisation et de son suivi.
 			</p>
 			<p>
@@ -17,104 +17,93 @@ const Cgu = () => {
 				implique l'acceptation intégrale et sans réserves des présentes
 				mentions légales par ce dernier.
 			</p>
+			<h2>Edition</h2>
 			<p>
-				<h2>Edition</h2>
-				<p>
-					Le Site est le site web du BDE de 42 Mulhouse. Il permet
-					l'accès aux évènements organisés par ce dernier et le
-					paiement des cotisations pour les étudiants connectés. Pour
-					l'Utilisateur non connecté, l'accès se résume principalement
-					à une vitrine du BDE et de ses évènements, ainsi qu'à la
-					prise de contact avec ce dernier.
-				</p>
-				<ul>
-					<li>
-						Directeur de la publication, ci après l'"Editeur" :
-						Allan ?
-					</li>
-					<li>Adresse : 30 Rue François Spoerry, 68100 Mulhouse</li>
-					<li>
-						Responsabilité technique, conception et développement :
-						### ### et Xxxxx XXXXXX
-					</li>
-				</ul>
+				Le Site est le site web du BDE (bureau des étudiants) de 42
+				Mulhouse. Il permet l'accès aux informations le concernant, aux
+				évènements organisés par ce dernier et le paiement des
+				cotisations pour les étudiants connectés. Pour l'Utilisateur non
+				connecté, l'accès se résume principalement à une vitrine du BDE
+				et de ses évènements, ainsi qu'à la prise de contact avec ce
+				dernier.
 			</p>
+			<ul>
+				<li>
+					Directeur de la publication, ci après l'"Editeur" :
+					<b>Allan ?</b>
+				</li>
+				<li>Adresse : 30 Rue François Spoerry, 68100 Mulhouse</li>
+				<li>
+					Responsabilité technique, conception et développement : ###
+					### et Xxxxx XXXXXX webmaster.bde42mulhouse@gmail.com
+				</li>
+			</ul>
+			<h2>Hébergement</h2>
 			<p>
-				<h2>Hébergement</h2>
-				<p>
-					L'hébergeur du Site est l'institut de formation 42 Mulhouse
-					à l'adresse suivante : 30 Rue François Spoerry, 68100
-					Mulhouse, pouvant être contactée au 0642694269 et joint par
-					mail via [mail de 42].
-				</p>
+				L'hébergeur du Site est l'institut de formation 42 Mulhouse à
+				l'adresse suivante : 30 Rue François Spoerry, 68100 Mulhouse,
+				pouvant être contactée au 0642694269 et joint par mail via{" "}
+				<b>[mail de 42]</b>.
 			</p>
+			<h2>Nom de domaine</h2>
 			<p>
-				<h2>Nom de domaine</h2>
-				<p>
-					Le nom de domaine est la propriété exclusive du BDE de 42
-					Mulhouse. La réservation du nom de domaine "bde42mulhouse.fr
-					" a été effectuée auprès de l'AFNIC conformément aux
-					dispositions en vigueur. L'attribution et le retrait des
-					noms de sous-domaines sont du ressort exclusif du BDE 42
-					Mulhouse.
-				</p>
+				Le nom de domaine est la propriété exclusive du BDE de 42
+				Mulhouse. La réservation du nom de domaine{" "}
+				<b>"bde42mulhouse.fr "</b> a été effectuée auprès de l'AFNIC
+				conformément aux dispositions en vigueur. L'attribution et le
+				retrait des noms de sous-domaines sont du ressort exclusif du
+				BDE 42 Mulhouse.
 			</p>
+			<h2>Accès au site</h2>
 			<p>
-				<h2>Accès au site</h2>
-				<p>
-					Le Site est accessible 7 jours sur 7 et 24 heures sur 24 en
-					tout lieu, excepté en cas de force majeure, de maintenance
-					annoncée ou non ou d'interruption annoncée, temporaire ou
-					définitive, des services. Dans ces cas, l'Editeur ne saurait
-					être tenu responsable de modifications, alterations,
-					interruptions ou suspensions du Site.
-				</p>
+				Le Site est accessible 7 jours sur 7 et 24 heures sur 24 en tout
+				lieu, excepté en cas de force majeure, de maintenance annoncée
+				ou non ou d'interruption annoncée, temporaire ou définitive, des
+				services. Dans ces cas, l'Editeur ne saurait être tenu
+				responsable de modifications, alterations, interruptions ou
+				suspensions du Site.
 			</p>
+			<h2>Propriété intelectuelle</h2>
 			<p>
-				<h2>Propriété intelectuelle</h2>
-				<p>
-					La présentation et le contenu du Site constituent, ensemble,
-					une œuvre protégée par les lois en vigueur sur la propriété
-					intellectuelle. Aucune exploitation commerciale,
-					reproduction, représentation, utilisation, adaptation,
-					modification, incorporation, traduction, partielle ou
-					intégrale des éléments de ce site ne pourra en être faite
-					sans l'accord préalable et écrit du BDE 42 Mulhouse, à
-					l'exception de l'utilisation pour un usage privé sous
-					réserve des dispositions différentes voire plus restrictives
-					du Code de la propriété intellectuelle. Tout le contenu du
-					Site, incluant, de façon non limitative, les graphismes,
-					images, textes, vidéos, animations, sons, logos, gifs et
-					icônes ainsi que leur mise en forme sont la propriété
-					exclusive du BDE 42 Mulhouse à l'exception des marques,
-					logos ou contenus appartenant à d'autres sociétés
-					partenaires ou auteurs. BDE 42 Mulhouse est identiquement
-					propriétaire des "droits des producteurs de bases de
-					données" visés au Livre III, Titre IV, du Code de la
-					Propriété Intellectuelle (loi n° 98-536 du 1er juillet 1998)
-					relative aux droits d'auteur et aux bases de données.
-				</p>
+				La présentation et le contenu du Site constituent, ensemble, une
+				œuvre protégée par les lois en vigueur sur la propriété
+				intellectuelle. Aucune exploitation commerciale, reproduction,
+				représentation, utilisation, adaptation, modification,
+				incorporation, traduction, partielle ou intégrale des éléments
+				de ce site ne pourra en être faite sans l'accord préalable et
+				écrit du BDE 42 Mulhouse, à l'exception de l'utilisation pour un
+				usage privé sous réserve des dispositions différentes voire plus
+				restrictives du Code de la propriété intellectuelle. Tout le
+				contenu du Site, incluant, de façon non limitative, les
+				graphismes, images, textes, vidéos, animations, sons, logos,
+				gifs et icônes ainsi que leur mise en forme sont la propriété
+				exclusive du BDE 42 Mulhouse à l'exception des marques, logos ou
+				contenus appartenant à d'autres sociétés partenaires ou auteurs.
+				BDE 42 Mulhouse est identiquement propriétaire des "droits des
+				producteurs de bases de données" visés au Livre III, Titre IV,
+				du Code de la Propriété Intellectuelle (loi n° 98-536 du 1er
+				juillet 1998) relative aux droits d'auteur et aux bases de
+				données.
 			</p>
+			<h2>Cookies</h2>
 			<p>
-				<h2>Cookies</h2>
-				<p>
-					Le Site utilise un cookie de session pour assurer son bon
-					fonctionnement. Aucune information concernant l'Utilisateur
-					du Site n'est récupérée. (Un cookie est un petit fichier
-					texte enregistré, lu par le navigateur de l'Utilisateur et
-					déposé par les sites internet qu'il visite. Quasiment tous
-					les sites utilisent des cookies pour bien fonctionner et
-					optimiser leur ergonomie et leurs fonctionnalités.
-				</p>
+				Le Site utilise un cookie de session pour assurer son bon
+				fonctionnement. Aucune information concernant l'Utilisateur du
+				Site n'est récupérée. (Un cookie est un petit fichier texte
+				enregistré, lu par le navigateur de l'Utilisateur et déposé par
+				les sites internet qu'il visite. Quasiment tous les sites
+				utilisent des cookies pour bien fonctionner et optimiser leur
+				ergonomie et leurs fonctionnalités.)
 			</p>
+			Il n’y a aucune exploitation commerciale, même partielle, des
+			données présentes sur le Site et elles ne pourront l’être sans
+			l’accord préalable et écrit de l’Association 42Mulhouse.
+			<h2>Nous contacter</h2>
 			<p>
-				Il n’y a aucune exploitation commerciale, même partielle, des
-				données présentes sur le Site et elles ne pourront l’être sans
-				l’accord préalable et écrit de l’Association 42Mulhouse.
-			</p>
-			<p>
-				<h2>Nous contacter</h2>
-				<p>Par email : contact.bde42mulhouse@gmail.com</p>
+				Par email via le{" "}
+				<a href="http://${global.config.api.authority}/contact">
+					formulaire de contact
+				</a>
 			</p>
 		</>
 	);
