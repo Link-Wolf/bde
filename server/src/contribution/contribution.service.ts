918bf9e8d4f5f087b93bff14ada9319eb6b4af54
laird-ikar <bguyot@student.42mulhouse.fr>
2022-08-11 11:00:47 +0200

    contribution go oui

--- server/src/contribution/contribution.service.ts
+++ server/src/contribution/contribution.service.ts
@@ -82,21 +82,19 @@ export class ContributionService {
 			this.logger.log(`Successfully created contribution for student ${contributionData.stud.login}`, requestMaker)
 		}
 		catch (error) {
-			await this.logger.error(`Failed to create contribution for student ${contributionData.stud.login} on database (${error})`, requestMaker);
-			throw new UnprocessableEntityException(`Failed to create contribution for student ${contributionData.stud.login} on database (${error})`);
+			await this.logger.error(`Failed to create contribution for student on database (${error})`, requestMaker);
+			throw new UnprocessableEntityException(`Failed to create contribution for student on database (${error})`);
 		}
 	}
 
 	async forceCreate(contributionData: ContributionDto, requestMaker: string): Promise<void> {
 		try {
-			console.log("try")
 			await this.contributionRepository.insert(contributionData);
 			this.logger.warn(`Successfully force-created contribution for student ${contributionData.stud.login}`, requestMaker)
 		}
 		catch (error) {
-			console.log("caatch")
-			await this.logger.error(`Failed to force-create contribution for student ${contributionData.stud.login} on database (${error})`, requestMaker);
-			throw new UnprocessableEntityException(`Failed to force-create contribution for student ${contributionData.stud.login} on database (${error})`);
+			await this.logger.error(`Failed to force-create contribution for student on database (${error})`, requestMaker);
+			throw new UnprocessableEntityException(`Failed to force-create contribution for student on database (${error})`);
 		}
 	}
 
