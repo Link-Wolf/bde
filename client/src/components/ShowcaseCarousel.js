49e28261d3ff6c6bf5d1d17774fb9f43b27f680c
iCARUS <94838879+sur4c1@users.noreply.github.com>
2022-08-26 16:10:25 +0200

    on our way to make that shit pretty

--- client/src/components/ShowcaseCarousel.js
+++ client/src/components/ShowcaseCarousel.js
@@ -1,39 +1,50 @@
 import {LazyLoadImage} from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
-import {Pagination} from "swiper";
+import {Pagination, Navigation} from "swiper";
 import {Swiper, SwiperSlide} from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/virtual";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import abel from "../assets/placeholders/abel.png";
 import yoyo from "../assets/placeholders/yohan.png";
 import caro from "../assets/placeholders/caroline.png";
 import iman from "../assets/placeholders/imane.png";
+import phol from "../assets/placeholders/longPlaceholder.jpg";
 
+import style from "../style/swiper.module.css";
 const ShowcaseCarousel = () => {
 	return (
 		<Swiper
 			slidesPerView={1}
-			spaceBetween={500}
+			spaceBetween={30}
 			centeredSlides={true}
 			pagination={{
 				clickable: true
 			}}
-			modules={[Pagination]}
-			className="mySwiper"
+			modules={[Pagination, Navigation]}
+			loop
+			navigation={true}
 		>
-			{[{src: abel}, {src: yoyo}, {src: caro}, {src: iman}].map(
-				(slideContent, index) => (
-					<SwiperSlide key={slideContent} virtualIndex={index}>
-						<LazyLoadImage
+			{[
+				{src: abel},
+				{src: yoyo},
+				{src: caro},
+				{src: iman},
+				{src: phol}
+			].map((slideContent, index) => (
+				<SwiperSlide key={index} virtualIndex={index} loop>
+					<div className={style.centerbitch}>
+						<img
 							src={slideContent.src}
-							height="400"
+							height="max"
 							effect="blur"
 						/>
-					</SwiperSlide>
-				)
-			)}
+					</div>
+				</SwiperSlide>
+			))}{" "}
+			<SwiperSlide loop>Bite</SwiperSlide>
 		</Swiper>
 	);
 };
