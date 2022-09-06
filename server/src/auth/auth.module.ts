5ef00ea34524bb69f72829bfb5c30a9cbc2aa150
iCARUS <94838879+sur4c1@users.noreply.github.com>
2022-09-06 08:36:46 +0200

    yeet old (and useless) login strats

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,10 +1,6 @@
 import { Module, HttpModule } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
 import { StudModule } from '../stud/stud.module';
 import { AuthService } from './auth.service';
-import { JwtStrategy } from './jwt.strategy';
-import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
 
 @Module({
@@ -12,13 +8,8 @@ import { AuthController } from './auth.controller';
 		StudModule, HttpModule.register({
 			timeout: 5000,
 			maxRedirects: 5,
-		}),
-		PassportModule,
-		JwtModule.register({
-			secret: "secretKey",//TODO: replace with config.json
-			signOptions: { expiresIn: '7 days' }
 		})],
-	providers: [AuthService, LocalStrategy, JwtStrategy],
+	providers: [AuthService],
 	exports: [AuthService],
 	controllers: [AuthController]
 })
