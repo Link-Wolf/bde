743c30e5bdc6f1d53f7fc9acbd99996c6b625d98
laird-ikar <bguyot@student.42mulhouse.fr>
2022-09-13 10:14:18 +0200

    fixing endpoint and getting the client token on client side

--- client/src/pages/body/Contribute.js
+++ client/src/pages/body/Contribute.js
@@ -1,3 +1,16 @@
-const Contribute = () => {};
+const Contribute = () => {
+	fetch(`http://${global.config.api.authority}/paypal/clientToken`, {
+		credentials: "include"
+	})
+		.then(response => {
+			if (!response.ok) {
+				throw new Error(
+					`This is an HTTP error: The status is ${response.status}`
+				);
+			}
+			return response.text();
+		})
+		.then(text => console.log(text));
+};
 
 export default Contribute;
