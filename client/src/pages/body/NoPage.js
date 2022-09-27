6f307684fc8af242b86ae536346bdc9bf18a0825
laird-ikar <bguyot@student.42mulhouse.fr>
2022-09-27 13:32:15 +0200

    jhfiu4foef

--- client/src/pages/body/NoPage.js
+++ client/src/pages/body/NoPage.js
@@ -1,7 +1,24 @@
-import {useEffect, React} from "react";
+import {useRef, React} from "react";
 import style from "../../style/NoPage.module.css";
 
 const NoPage = () => {
+	// document.mousemove(function(event) {
+	// 	// document.getElementsByClassName(".torch").forEach(item => {
+	// 	// 	item.css({
+	// 	// 		top: event.pageY,
+	// 	// 		left: event.pageX
+	// 	// 	});
+	// 	// });
+	// 	console.log("mouve");
+	// });
+	const mouseX = useRef();
+	const mouseY = useRef();
+
+	document.addEventListener("mousemove", e => {
+		mouseY.current = e.pageY;
+		mouseX.current = e.pageX;
+	});
+
 	return (
 		<>
 			<div className={style.text}>
@@ -12,7 +29,10 @@ const NoPage = () => {
 					in here
 				</h3>
 			</div>
-			<div className={style.torch}></div>
+			<div
+				className={style.torch}
+				style={{top: `${mouseX.current}`, bottom: `${mouseY.current}`}}
+			></div>
 		</>
 	);
 };
