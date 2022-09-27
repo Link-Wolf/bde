a1b8d822562676db94cebce426c33b73e73374e2
Link <wolflink6859@gmail.com>
2022-09-27 14:09:22 +0200

    background nopage

--- client/src/pages/body/NoPage.js
+++ client/src/pages/body/NoPage.js
@@ -2,15 +2,6 @@ import {useRef, React} from "react";
 import style from "../../style/NoPage.module.css";
 
 const NoPage = () => {
-	// document.mousemove(function(event) {
-	// 	// document.getElementsByClassName(".torch").forEach(item => {
-	// 	// 	item.css({
-	// 	// 		top: event.pageY,
-	// 	// 		left: event.pageX
-	// 	// 	});
-	// 	// });
-	// 	console.log("mouve");
-	// });
 	const mouseX = useRef();
 	const mouseY = useRef();
 
@@ -22,12 +13,7 @@ const NoPage = () => {
 	return (
 		<>
 			<div className={style.text}>
-				<h1>404</h1>
-				<h2>Uh, Ohh</h2>
-				<h3>
-					Sorry we cant find what you are looking for 'cuz its so dark
-					in here
-				</h3>
+				<h3>test</h3>
 			</div>
 			<div
 				className={style.torch}
