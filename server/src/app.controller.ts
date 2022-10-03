9b8daa7f41af6a5c45a637039fc0c0541e45673d
iCARUS <94838879+sur4c1@users.noreply.github.com>
2022-10-03 16:39:01 +0200

    des trucs

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -5,6 +5,30 @@ import { StudService } from './stud/stud.service';
 export class AppController {
 	constructor(private studService: StudService) { }
 
+	@Get('session/admin')
+	async getAdminSession(@Session() session: Record<string, any>) {
+		if (session.login === undefined)
+			return {
+				clearance: session.clearance ? session.clearance : 0,
+				image_url: session.image_url ? session.image_url : -42,
+				login: session.login ? session.login : -42,
+				firstname: session.firstname ? session.firstname : "",
+				mail: session.mail ? session.mail : "",
+				lastname: session.lastname ? session.lastname : "",
+			};
+		let stud = await this.studService.findOne(session.login, session.login);
+		session.clearance = stud.clearance;
+		console.log(stud)
+		return {
+			clearance: stud.clearance ? stud.clearance : 0,
+			image_url: session.image_url ? session.image_url : -42,
+			login: session.login ? session.login : -42,
+			firstname: session.firstname ? session.firstname : "",
+			mail: session.mail ? session.mail : "",
+			lastname: session.lastname ? session.lastname : "",
+		}
+	}
+
 	@Get('session')
 	async getSession(@Session() session: Record<string, any>) {
 		return {
@@ -17,17 +41,5 @@ export class AppController {
 		}
 	}
 
-	@Get('session/admin')
-	async getAdminSession(@Session() session: Record<string, any>) {
-		let stud = await this.studService.findOne(session.login, session.login);
-		session.clearance = stud.clearance;
-		return {
-			clearance: stud.clearance ? stud.clearance : 0,
-			image_url: session.image_url ? session.image_url : -42,
-			login: session.login ? session.login : -42,
-			firstname: session.firstname ? session.firstname : "",
-			mail: session.mail ? session.mail : "",
-			lastname: session.lastname ? session.lastname : "",
-		}
-	}
+
 }
