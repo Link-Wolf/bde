c955ee4fac48c4760f781967d1a29ed797c20ae7
iCARUS <94838879+sur4c1@users.noreply.github.com>
2022-10-11 09:09:58 +0200

    adding mail loading

---
+++ client/src/components/Loading.js
@@ -0,0 +1,9 @@
+import style from "../style/Loading.module.scss";
+
+import gear from "../assets/animations/gear_clockwise.gif";
+
+const Loading = () => {
+	return <img src={gear} />;
+};
+
+export default Loading;
