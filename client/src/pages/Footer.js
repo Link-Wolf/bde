370e3a6f5a021dd581965ca3e51e9bd8f89e7c6e
iCARUS <94838879+sur4c1@users.noreply.github.com>
2022-10-17 08:43:38 +0200

    yeeting insta logo (lazyload de mderde)

--- client/src/pages/Footer.js
+++ client/src/pages/Footer.js
@@ -4,21 +4,18 @@ import twitter_logo from "../assets/logos/twitter.png";
 import discord_logo from "../assets/logos/discord.png";
 import instagram_logo from "../assets/logos/instagram.png";
 
-import {LazyLoadImage} from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/blur.css";
-
 function Footer() {
 	return (
 		<footer className={style.footer} id="footer">
 			<div>
-				<LazyLoadImage
+				<img
 					height="auto"
 					src={twitter_logo}
 					width="auto"
 					effect="blur"
 					hidden
 				/>
-				<LazyLoadImage
+				<img
 					height="auto"
 					src={discord_logo}
 					width="auto"
@@ -26,7 +23,7 @@ function Footer() {
 					hidden
 				/>
 				<a href="https://www.instagram.com/bde_42mulhouse/">
-					<LazyLoadImage
+					<img
 						height="auto"
 						src={instagram_logo}
 						width="auto"
