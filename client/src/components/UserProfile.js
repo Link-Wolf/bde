cab74e8147c5755a5cd7ec276c8d3f2f84902728
iCARUS <94838879+sur4c1@users.noreply.github.com>
2022-10-21 09:29:13 +0200

    user profile + docker compose (need commit)

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -154,13 +154,157 @@ const Identity = props => {
 				</li>
 				<li>
 					Membre depuis le{" "}
-					{new Intl.DateTimeFormat("fr-FR", {
-						day: "numeric",
-						month: "short",
-						year: "numeric"
-					}).format(new Date(props.stud.joinDate))}
+					<b>
+						{new Intl.DateTimeFormat("fr-FR", {
+							day: "numeric",
+							month: "short",
+							year: "numeric"
+						}).format(new Date(props.stud.joinDate))}
+					</b>
 				</li>
-				<li>Jours de cotisation restants : {props.blackHole}</li>
+				<li>
+					Jours de cotisation restants : <b>{props.blackHole}</b>
+				</li>
+				<li>
+					{props.blackHole > 0 ? (
+						props.blackHole > 30 ? (
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 14 14"
+							>
+								<path
+									stroke="#000000"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M7 13.5C10.5899 13.5 13.5 10.5899 13.5 7C13.5 3.41015 10.5899 0.5 7 0.5C3.41015 0.5 0.5 3.41015 0.5 7C0.5 10.5899 3.41015 13.5 7 13.5Z"
+								/>
+								<path
+									stroke="#000000"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M3.7 8C4.2 9.8 6.2 10.9 8 10.4C9.1 10 10 9.1 10.3 8"
+								/>
+								<path
+									stroke="#000000"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M4.8 5.45C4.66193 5.45 4.55 5.33807 4.55 5.2C4.55 5.06193 4.66193 4.95 4.8 4.95"
+								/>
+								<path
+									stroke="#000000"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M4.8 5.45C4.93807 5.45 5.05 5.33807 5.05 5.2C5.05 5.06193 4.93807 4.95 4.8 4.95"
+								/>
+								<g>
+									<path
+										stroke="#000000"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										d="M9.2 5.45C9.06193 5.45 8.95 5.33807 8.95 5.2C8.95 5.06193 9.06193 4.95 9.2 4.95"
+									/>
+									<path
+										stroke="#000000"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										d="M9.2 5.45C9.33807 5.45 9.45 5.33807 9.45 5.2C9.45 5.06193 9.33807 4.95 9.2 4.95"
+									/>
+								</g>
+							</svg>
+						) : (
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 14 14"
+							>
+								<path
+									stroke="#000000"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M3.5 9.5H10.5"
+								/>
+								<path
+									stroke="#000000"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M7 13.5C10.5899 13.5 13.5 10.5899 13.5 7C13.5 3.41015 10.5899 0.5 7 0.5C3.41015 0.5 0.5 3.41015 0.5 7C0.5 10.5899 3.41015 13.5 7 13.5Z"
+								/>
+								<path
+									stroke="#000000"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M4.8 5.45C4.66193 5.45 4.55 5.33807 4.55 5.2C4.55 5.06193 4.66193 4.95 4.8 4.95"
+								/>
+								<path
+									stroke="#000000"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M4.8 5.45C4.93807 5.45 5.05 5.33807 5.05 5.2C5.05 5.06193 4.93807 4.95 4.8 4.95"
+								/>
+								<g>
+									<path
+										stroke="#000000"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										d="M9.2 5.45C9.06193 5.45 8.95 5.33807 8.95 5.2C8.95 5.06193 9.06193 4.95 9.2 4.95"
+									/>
+									<path
+										stroke="#000000"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										d="M9.2 5.45C9.33807 5.45 9.45 5.33807 9.45 5.2C9.45 5.06193 9.33807 4.95 9.2 4.95"
+									/>
+								</g>
+							</svg>
+						)
+					) : (
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 14 14"
+						>
+							<path
+								stroke="#000000"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M7 13.5C10.5899 13.5 13.5 10.5899 13.5 7C13.5 3.41015 10.5899 0.5 7 0.5C3.41015 0.5 0.5 3.41015 0.5 7C0.5 10.5899 3.41015 13.5 7 13.5Z"
+							/>
+							<path
+								stroke="#000000"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M3.7 10.5C4.2 8.7 6.1 7.6 8 8.1C9.1 8.4 10 9.3 10.4 10.5"
+							/>
+							<path
+								stroke="#000000"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M4.85 5.45C4.71193 5.45 4.6 5.33807 4.6 5.2C4.6 5.06193 4.71193 4.95 4.85 4.95"
+							/>
+							<path
+								stroke="#000000"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M4.85 5.45C4.98807 5.45 5.1 5.33807 5.1 5.2C5.1 5.06193 4.98807 4.95 4.85 4.95"
+							/>
+							<g>
+								<path
+									stroke="#000000"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M9.24999 5.45C9.11192 5.45 8.99999 5.33807 8.99999 5.2C8.99999 5.06193 9.11192 4.95 9.24999 4.95"
+								/>
+								<path
+									stroke="#000000"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M9.24999 5.45C9.38806 5.45 9.49999 5.33807 9.49999 5.2C9.49999 5.06193 9.38806 4.95 9.24999 4.95"
+								/>
+							</g>
+						</svg>
+					)}
+				</li>
 			</ul>
 		</div>
 	);
