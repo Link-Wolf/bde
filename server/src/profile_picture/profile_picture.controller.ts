e846fae7d1de62d0e4ef8e34fc029c09b8fc490b
iCARUS <94838879+sur4c1@users.noreply.github.com>
2022-10-25 12:49:33 +0200

    "fixing" scroll (je deteste le css)

--- server/src/profile_picture/profile_picture.controller.ts
+++ server/src/profile_picture/profile_picture.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Session } from '@nestjs/common';
 
 @Controller('profile-picture')
-export class ProfilePictureController {}
+export class ProfilePictureController {
+	@Get(':login')
+	getLogin(@Param('login') login: string, @Session() session: Record<string, any>) {
+		// this.profilePictureService.getPP(login, session.login);
+	}
+}
