f95eacf90d85882006f7c3e5a036f27a752c6f10
iCARUS <94838879+sur4c1@users.noreply.github.com>
2022-10-25 17:42:04 +0200

    home made pop up moche

--- client/src/components/ConfirmModal.js
+++ client/src/components/ConfirmModal.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from "react";
 import useConfirm from "./useConfirm";
-import {Modal, ModalHeader, ModalBody, ModalFooter, Button} from "reactstrap";
+import style from "../style/ConfirmModal.module.scss";
 
 const ConfirmModal = () => {
 	const {prompt = "", isOpen = false, proceed, cancel} = useConfirm();
@@ -16,18 +16,20 @@ const ConfirmModal = () => {
 	}, [proceed, isOpen]);
 
 	return (
-		<Modal isOpen={isOpen} toggle={cancel} fade>
-			<ModalHeader>Confirm</ModalHeader>
-			<ModalBody>{prompt}</ModalBody>
-			<ModalFooter>
-				<Button color="primary" onClick={proceed} className="ml-4">
-					Ok
-				</Button>
-				<Button color="secondary" onClick={cancel}>
-					Cancel
-				</Button>
-			</ModalFooter>
-		</Modal>
+		isOpen && (
+			<div className={style.popUpContainer}>
+				<div></div>
+				<div className={style.popUp}>
+					<h1>Confirm</h1>
+					<p>{prompt}</p>
+					<div className={style.buttons}>
+						<button onClick={proceed}>Ok</button>
+						<button onClick={cancel}>Cancel</button>
+					</div>
+				</div>
+				<div></div>
+			</div>
+		)
 	);
 };
 
