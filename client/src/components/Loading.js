02e2601e5881db459ea774448cee2f4d420f49b3
Link <wolflink6859@gmail.com>
2022-10-26 16:20:20 +0200

    differents size loading

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,9 +1,25 @@
 import style from "../style/Loading.module.scss";
 
 import gear from "../assets/animations/gear_clockwise.gif";
+import gear_small from "../assets/animations/gear_clockwise.gif";
+import gear_medium from "../assets/animations/gear_clockwise.gif";
+import gear_micro from "../assets/animations/gear_clockwise.gif";
 
 const Loading = () => {
 	return <img src={gear} />;
 };
 
+const LoadingSmall = () => {
+	return <img src={gear_small} />;
+};
+
+const LoadingMedium = () => {
+	return <img src={gear_medium} />;
+};
+
+const LoadingMicro = () => {
+	return <img src={gear_micro} />;
+};
+
 export default Loading;
+export {Loading, LoadingMedium, LoadingSmall, LoadingMicro};
