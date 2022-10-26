8b1c658217bec6c79395240f3bf9febda7c9bba4
laird-ikar <bguyot@student.42mulhouse.fr>
2022-10-26 13:38:36 +0200

    header fixixixiixix

--- client/src/pages/Header.js
+++ client/src/pages/Header.js
@@ -138,7 +138,44 @@ function Header() {
 					<DropdownUser />
 				</div>
 				<div className={style.burger}>
-					<img src={burger} onClick={toggleBurgerMenu} />
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 14 14"
+						onClick={toggleBurgerMenu}
+					>
+						<g>
+							<line
+								x1={13.5}
+								y1={2}
+								x2={0.5}
+								y2={2}
+								fill="none"
+								stroke="var(--white)"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							/>
+							<line
+								x1={13.5}
+								y1={7}
+								x2={0.5}
+								y2={7}
+								fill="none"
+								stroke="var(--white)"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							/>
+							<line
+								x1={13.5}
+								y1={12}
+								x2={0.5}
+								y2={12}
+								fill="none"
+								stroke="var(--white)"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							/>
+						</g>
+					</svg>
 				</div>
 			</div>
 		</header>
