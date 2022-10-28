4f8688ab4f3979a44637d764051ebfcacc4a84bb
Link <wolflink6859@gmail.com>
2022-10-28 13:45:56 +0200

    404page phone wip

--- client/src/pages/body/NoPage.js
+++ client/src/pages/body/NoPage.js
@@ -1,6 +1,8 @@
 import {useState, React} from "react";
 import style from "../../style/NoPage.module.scss";
 
+import boat from "../../assets/animations/nopage_tel.gif";
+
 const NoPage = () => {
 	const [mouseX, setMouseX] = useState(0);
 	const [mouseY, setMouseY] = useState(0);
@@ -11,13 +13,16 @@ const NoPage = () => {
 	});
 
 	return (
-		<>
+		<div className={style.nopage}>
 			<div className={style.body}>
 				<div className={style.text}>
 					<h1>404</h1>
 					<h2>Page not found</h2>
 					<h3>La Frégate s'est perdue...</h3>
 					<h3>tu es sur de ton chemin ?</h3>
+				</div>
+				<div className={boat}>
+					<img src={boat} />
 				</div>
 				<div
 					className={style.torch}
@@ -28,7 +33,7 @@ const NoPage = () => {
 					}}
 				></div>
 			</div>
-		</>
+		</div>
 	);
 };
 
