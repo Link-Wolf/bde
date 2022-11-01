cb28892fcacb52d7fc4025a838bd1d2a817b37df
Link <94838493+Link-Wolf@users.noreply.github.com>
2022-11-01 14:52:22 +0100

    quick update navbar admin

--- client/src/components/AdminNavbar.js
+++ client/src/components/AdminNavbar.js
@@ -28,224 +28,23 @@ const AdminNavbar = () => {
 				isDirection: true,
 				clearance: 42
 			})
-		})
-			.then(() => {
-				fetch(`${process.env.REACT_APP_API_URL}/stud`, {
-					headers: {"Content-Type": "application/json"},
-					method: "POST",
-					credentials: "include",
-					body: JSON.stringify({
-						login: "Link",
-						firstname: "Xxxxx",
-						lastname: "Xxxxxx",
-						email: "Link@student.42mulhouse.fr",
-						true_email:
-							"[90,77,52,54,142,29,121,85,8,1,225,158,17,120,133,71,97,165,130,30,188,78,231,250,2,60,11,2,99,157,38,224]",
-						isDirection: true,
-						clearance: 42
-					})
-				});
-			})
-			.then(() => {
-				fetch(`${process.env.REACT_APP_API_URL}/stud`, {
-					headers: {"Content-Type": "application/json"},
-					method: "POST",
-					credentials: "include",
-					body: JSON.stringify({
-						login: "yoyostud",
-						firstname: "Yohan",
-						lastname: "Thollet",
-						email: "",
-						true_email: "",
-						isDirection: false,
-						clearance: 7
-					})
-				});
-			})
-			.then(() => {
-				fetch(`${process.env.REACT_APP_API_URL}/goodies`, {
-					headers: {"Content-Type": "application/json"},
-					method: "POST",
-					credentials: "include",
-					body: JSON.stringify({
-						name: "Hoodie Premium+ Noir brodé",
-						cost: 42,
-						desc:
-							"Hoodie noir doublé avec logo 42 Mulhouse brodé sur le coeur en petit et en grand dans le dos",
-						available: true
-					})
-				});
-			})
-			.then(() => {
-				fetch(`${process.env.REACT_APP_API_URL}/goodies`, {
-					headers: {"Content-Type": "application/json"},
-					method: "POST",
-					credentials: "include",
-					body: JSON.stringify({
-						name: "Casquette noire brodée",
-						cost: 9.99,
-						desc: "Casquette noire avec logo 42 Mulhouse brodé",
-						available: true
-					})
-				});
-			})
-			.then(() => {
-				fetch(`${process.env.REACT_APP_API_URL}/goodies`, {
-					headers: {"Content-Type": "application/json"},
-					method: "POST",
-					credentials: "include",
-					body: JSON.stringify({
-						name: "T-shirt noir brodé",
-						cost: 13.99,
-						desc:
-							"T-shirt noir avec logo 42 Mulhouse brodé sur le coeur",
-						available: false
-					})
-				});
-			})
-			.then(() => {
-				fetch(`${process.env.REACT_APP_API_URL}/event`, {
-					headers: {"Content-Type": "application/json"},
-					method: "POST",
-					credentials: "include",
-					body: JSON.stringify({
-						name: "Loup Garou",
-						cost: 5,
-						premium_cost: 3,
-						nb_places: 15,
-						begin_date: "06-13-2022",
-						end_date: "06-13-2022",
-						desc: "L'uwu garou du vendredi matin en jeu de cartes",
-						place: "cantina",
-						isOutside: false,
-						consos: true,
-						for_pool: true,
-						available: false,
-						sponso: false,
-						thumbnail_filename: "assets/thumbnails/events/1.jpeg"
-					})
-				});
-			})
-			.then(() => {
-				fetch(`${process.env.REACT_APP_API_URL}/event`, {
-					headers: {"Content-Type": "application/json"},
-					method: "POST",
-					credentials: "include",
-					body: JSON.stringify({
-						name: "Tournoi Echecs",
-						cost: 0,
-						nb_places: -42,
-						begin_date: "06-13-2022",
-						end_date: "06-14-2023",
-						place: "cantina",
-						isOutside: false,
-						consos: true,
-						for_pool: false,
-						available: false,
-						sponso: false,
-						thumbnail_filename: "assets/thumbnails/events/2.jpeg"
-					})
-				});
-			})
-			.then(() => {
-				fetch(`${process.env.REACT_APP_API_URL}/event`, {
-					headers: {"Content-Type": "application/json"},
-					method: "POST",
-					credentials: "include",
-					body: JSON.stringify({
-						name: "Ventriglisse",
-						cost: 15,
-						nb_places: 50,
-						begin_date: "06-13-2022",
-						place: "Derriere 42",
-						desc: "Ventriglisse sur nos corps huilés et musculeux",
-						isOutside: false,
-						consos: true,
-						for_pool: false,
-						available: true,
-						sponso: false,
-						thumbnail_filename: "assets/thumbnails/events/3.jpeg"
-					})
-				});
-			})
-			.then(() => {
-				fetch(`${process.env.REACT_APP_API_URL}/event`, {
-					headers: {"Content-Type": "application/json"},
-					method: "POST",
-					credentials: "include",
-					body: JSON.stringify({
-						name: "Laser game",
-						cost: 7,
-						premium_cost: 5,
-						nb_places: 19,
-						begin_date: "06-13-2022",
-						end_date: "06-15-2022",
-						place: "42 rue de la norminette 69420 Mulhouse",
-						desc: "piou piou avec des lasers",
-						isOutside: true,
-						consos: false,
-						for_pool: true,
-						available: false,
-						sponso: false,
-						thumbnail_filename: "assets/thumbnails/events/4.jpeg"
-					})
-				});
-			})
-			.then(() => {
-				fetch(`${process.env.REACT_APP_API_URL}/contribution`, {
-					headers: {"Content-Type": "application/json"},
-					method: "POST",
-					credentials: "include",
-					body: JSON.stringify({
-						studLogin: "yoyostud"
-					})
-				});
-			})
-			.then(() => {
-				fetch(`${process.env.REACT_APP_API_URL}/contribution`, {
-					headers: {"Content-Type": "application/json"},
-					method: "POST",
-					credentials: "include",
-					body: JSON.stringify({
-						studLogin: "Link",
-						begin_date: "12-12-2022"
-					})
-				});
+		}).then(() => {
+			fetch(`${process.env.REACT_APP_API_URL}/stud`, {
+				headers: {"Content-Type": "application/json"},
+				method: "POST",
+				credentials: "include",
+				body: JSON.stringify({
+					login: "Link",
+					firstname: "Xxxxx",
+					lastname: "Xxxxxx",
+					email: "Link@student.42mulhouse.fr",
+					true_email:
+						"[90,77,52,54,142,29,121,85,8,1,225,158,17,120,133,71,97,165,130,30,188,78,231,250,2,60,11,2,99,157,38,224]",
+					isDirection: true,
+					clearance: 42
+				})
 			});
-	};
-	const tiniDb = async () => {
-		const confirm = await isConfirmed(
-			`T'es archi sur de vouloir enculer la database ???`
-		);
-		if (!confirm) return;
-		fetch(`${process.env.REACT_APP_API_URL}/stud`, {
-			credentials: "include",
-			method: "DELETE"
-		})
-			.then(() => {
-				fetch(`${process.env.REACT_APP_API_URL}/event`, {
-					credentials: "include",
-					method: "DELETE"
-				});
-			})
-			.then(() => {
-				fetch(`${process.env.REACT_APP_API_URL}/goodies`, {
-					credentials: "include",
-					method: "DELETE"
-				});
-			})
-			.then(() => {
-				fetch(`${process.env.REACT_APP_API_URL}/inscription`, {
-					credentials: "include",
-					method: "DELETE"
-				});
-			})
-			.then(() => {
-				fetch(`${process.env.REACT_APP_API_URL}/contribution`, {
-					credentials: "include",
-					method: "DELETE"
-				});
-			});
+		});
 	};
 
 	useEffect(() => {
@@ -385,10 +184,7 @@ const AdminNavbar = () => {
 				{session.clearance >= 42 && (
 					<div id={style.buttonContainer}>
 						<button id={style.bigYellowButton} onClick={initDb}>
-							Init DB
-						</button>
-						<button id={style.bigRedButton} onClick={tiniDb}>
-							Yeet DB
+							Init sUS
 						</button>
 					</div>
 				)}
