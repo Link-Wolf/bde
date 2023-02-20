14dd6d7a2b059913968df6d98e751d9dd8474e4f
Link <94838493+Link-Wolf@users.noreply.github.com>
2023-02-20 14:43:22 +0100

    misc mv some files, css to scss, del some useless other files

--- client/src/App.js
+++ client/src/App.js
@@ -3,36 +3,35 @@
 import ConfirmContextProvider from "./components/ConfirmContextProvider";
 import ConfirmModal from "./components/ConfirmModal";
 import NotificationContextProvider from "./components/NotificationContextProvider";
-import {ReactNotifications} from "react-notifications-component";
+import { ReactNotifications } from "react-notifications-component";
 
 import "./style/App.scss";
 import "swiper/css/bundle";
-import "./style/swiper.css";
 
 import "react-notifications-component/dist/theme.css";
 import Routage from "./pages/Routage";
 
 function App() {
-	window._ = arg => {
-		fetch(`${process.env.REACT_APP_API_URL}/stud/_`, {
-			method: "POST",
-			credentials: "include",
-			headers: {"Content-Type": "application/json"},
-			body: JSON.stringify({pass: arg})
-		});
-	};
+    window._ = (arg) => {
+        fetch(`${process.env.REACT_APP_API_URL}/stud/_`, {
+            method: "POST",
+            credentials: "include",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ pass: arg }),
+        });
+    };
 
-	return (
-		<NotificationContextProvider>
-			<ConfirmContextProvider>
-				<div className={`App`}>
-					<ReactNotifications />
-					<Routage />
-					<ConfirmModal />
-				</div>
-			</ConfirmContextProvider>
-		</NotificationContextProvider>
-	);
+    return (
+        <NotificationContextProvider>
+            <ConfirmContextProvider>
+                <div className={`App`}>
+                    <ReactNotifications />
+                    <Routage />
+                    <ConfirmModal />
+                </div>
+            </ConfirmContextProvider>
+        </NotificationContextProvider>
+    );
 }
 
 export default App;
