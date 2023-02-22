19e7d4dc5e3898967a761f0942c4c54cf69d149b
Link-Wolf <wolflink6859@gmail.com>
2023-02-22 14:00:21 +0100

    yeet all isAdin things (i pray)

--- server/src/stud/stud.pipe.ts
+++ server/src/stud/stud.pipe.ts
@@ -4,12 +4,6 @@ import { Injectable, PipeTransform } from "@nestjs/common";
 @Injectable()
 export class StudDtoPipe implements PipeTransform {
 	transform(value: any, _metadata: ArgumentMetadata) {
-		if ("isAdmin" in value) {
-			if (value.isAdmin === "true")
-				value.isAdmin = true;
-			else
-				value.isAdmin = false;
-		}
 		return value;
 	}
 }
