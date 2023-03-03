e339b95633390eb2b410f8b29bc45de21246513a
Link-Wolf <wolflink6859@gmail.com>
2023-03-03 14:07:07 +0100

    form field

---
+++ client/src/components/FormField.js
@@ -0,0 +1,31 @@
+import style from "../style/FormField.module.scss";
+
+const FormField = ({
+    label,
+    name,
+    value,
+    onChange,
+    type = "text",
+    placeholder = "",
+    disabled = false,
+    labelPosition = "left",
+    className = "",
+    ...rest
+}) => (
+    <div className={`${style.formField} ${className}`}>
+        {labelPosition === "left" && <label htmlFor={name}>{label}</label>}
+        <input
+            type={type}
+            name={name}
+            value={value}
+            onChange={onChange}
+            placeholder={placeholder}
+            className={style.formInput}
+            disabled={disabled}
+            {...rest}
+        />
+        {labelPosition === "right" && <label htmlFor={name}>{label}</label>}
+    </div>
+);
+
+export default FormField;
