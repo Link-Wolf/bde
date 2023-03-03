4ae51b10d246416fc6bd709e59816ba7457c8c8e
Link <94838493+Link-Wolf@users.noreply.github.com>
2023-03-03 17:29:23 +0100

    add info if perma event

--- client/src/components/EventToken.js
+++ client/src/components/EventToken.js
@@ -1,5 +1,5 @@
-import {useEffect, useState} from "react";
-import {NotificationManager} from "react-notifications";
+import { useEffect, useState } from "react";
+import { NotificationManager } from "react-notifications";
 import styleBasic from "../style/EventToken.module.scss";
 import styleUser from "../style/EventTokenUser.module.scss";
 import conso from "../assets/logos/consos.svg";
@@ -10,88 +10,95 @@ import fadedSponso from "../assets/logos/fadedSponso.svg";
 import fadedPool from "../assets/logos/fadedPool.svg";
 import dateTime from "../assets/logos/date.svg";
 
-const EventToken = param => {
-	const [imgSrc, setImgSrc] = useState(null);
-	let style;
-	if (param.user) style = styleUser;
-	else style = styleBasic;
-	useEffect(() => {
-		fetch(
-			`${process.env.REACT_APP_API_URL}/event/${param.event.id}/thumbnail`,
-			{
-				credentials: "include"
-			}
-		)
-			.then(response => {
-				if (!response.ok) {
-					throw new Error(
-						`This is an HTTP error: The status is` +
-							` ${response.status}`
-					);
-				}
-				return response.blob();
-			})
-			.then(blob => {
-				setImgSrc(URL.createObjectURL(blob));
-			})
-			.catch(function(error) {
-				NotificationManager.error(
-					"Une erreur est survenue, réessayez plus tard (si le problème subsiste contactez nous)",
-					"Erreur",
-					5000
-				);
-			});
-	}, []);
+const EventToken = (param) => {
+    const [imgSrc, setImgSrc] = useState(null);
+    let style;
+    if (param.user) style = styleUser;
+    else style = styleBasic;
+    useEffect(() => {
+        fetch(
+            `${process.env.REACT_APP_API_URL}/event/${param.event.id}/thumbnail`,
+            {
+                credentials: "include",
+            }
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(
+                        `This is an HTTP error: The status is` +
+                            ` ${response.status}`
+                    );
+                }
+                return response.blob();
+            })
+            .then((blob) => {
+                setImgSrc(URL.createObjectURL(blob));
+            })
+            .catch(function (error) {
+                NotificationManager.error(
+                    "Une erreur est survenue, réessayez plus tard (si le problème subsiste contactez nous)",
+                    "Erreur",
+                    5000
+                );
+            });
+    }, []);
 
-	return (
-		<>
-			<a
-				onClick={() => {
-					param.setPopUpEvent(param.event.id);
-				}}
-			>
-				<div className={style.eventTokenContainer}>
-					<img alt="" src={imgSrc} />
-					<div>
-						<h1>{param.event.name}</h1>
-						<div>
-							{new Intl.DateTimeFormat("fr-FR", {
-								day: "numeric",
-								month: "short",
-								year: "numeric"
-							}).format(new Date(param.event.begin_date))}
-						</div>
-						<ul>
-							<li>
-								<img alt=""
-									src={
-										param.event.consos ? conso : fadedConso
-									}
-								/>
-							</li>
-							<li>
-								<img alt=""
-									src={
-										param.event.for_pool ? pool : fadedPool
-									}
-								/>
-							</li>
-							<li>
-								<img alt=""
-									src={
-										param.event.sponso
-											? sponso
-											: fadedSponso
-									}
-								/>
-							</li>
-						</ul>
-					</div>
-				</div>
-				<hr />
-			</a>
-		</>
-	);
+    return (
+        <>
+            <a
+                onClick={() => {
+                    param.setPopUpEvent(param.event.id);
+                }}
+            >
+                <div className={style.eventTokenContainer}>
+                    <img alt="" src={imgSrc} />
+                    <div>
+                        <h1>{param.event.name}</h1>
+                        <div>
+                            {!param.event.end_date ? (
+                                <label>Permanent</label>
+                            ) : (
+                                new Intl.DateTimeFormat("fr-FR", {
+                                    day: "numeric",
+                                    month: "short",
+                                    year: "numeric",
+                                }).format(new Date(param.event.begin_date))
+                            )}
+                        </div>
+                        <ul>
+                            <li>
+                                <img
+                                    alt=""
+                                    src={
+                                        param.event.consos ? conso : fadedConso
+                                    }
+                                />
+                            </li>
+                            <li>
+                                <img
+                                    alt=""
+                                    src={
+                                        param.event.for_pool ? pool : fadedPool
+                                    }
+                                />
+                            </li>
+                            <li>
+                                <img
+                                    alt=""
+                                    src={
+                                        param.event.sponso
+                                            ? sponso
+                                            : fadedSponso
+                                    }
+                                />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <hr />
+            </a>
+        </>
+    );
 };
 
 export default EventToken;
